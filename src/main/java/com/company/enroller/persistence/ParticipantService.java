43b7a4072c0f9e188da3560f851db7451db4ab9a
student25 <student25@pcoip.ki.agh.edu.pl>
2023-04-15 14:16:14 +0200

    enroller_sortBy_sortOrder_key

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,7 +2,7 @@ package com.company.enroller.persistence;
 
 import com.company.enroller.model.Participant;
 import org.hibernate.Transaction;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
@@ -18,6 +18,23 @@ public class ParticipantService {
 
 	public Collection<Participant> getAll() {
 		return connector.getSession().createCriteria(Participant.class).list();
+	}
+
+	public Collection<Participant> getAll(String sortBy, String sortOrder, String key) {
+		String hql = " FROM Participant";
+		if (!key.isEmpty()) {
+			hql += " WHERE login LIKE '%" + key + "%' ";
+		}
+
+		if (sortBy.equals("login")) {
+			hql += " ORDER by login";
+			if (sortOrder.equals("ASC") || sortOrder.equals("DESC")) {
+			hql += " " + sortOrder;
+			}
+
+		}
+		Query query = connector.getSession().createQuery(hql);
+		return query.list();
 	}
 
 	public Participant findByLogin(String login) {
